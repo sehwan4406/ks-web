a95706ac586afae18f50ee3f595d6c34dde7e7f1
Sehwan Park <p0424sh@gmail.com>
2020-04-12 23:17:13 +0900

    init

---
+++ src/test/java/com/sehwan/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehwan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
